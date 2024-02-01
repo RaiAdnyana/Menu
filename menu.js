c664de0b96846aa967a522bc0c3aebb23c11d4f6
Rai Adnyana <142960649+RaiAdnyana@users.noreply.github.com>
2024-02-02 07:53:02 +0800

    Add files via upload

---
+++ menu.js
@@ -0,0 +1,71 @@
+// A
+const hidden1 = document.querySelector('.keterangan-1a input');
+const selengkapnya1 = document.querySelector('.keterangan-2a')
+
+hidden1.addEventListener('click', function() {
+    selengkapnya1.classList.toggle('slide');
+})
+
+// B
+const hidden2 = document.querySelector('.keterangan-1b input');
+const selengkapnya2 = document.querySelector('.keterangan-2b')
+
+hidden2.addEventListener('click', function() {
+    selengkapnya2.classList.toggle('slide');
+})
+
+// C
+const hidden3 = document.querySelector('.keterangan-1c input');
+const selengkapnya3 = document.querySelector('.keterangan-2c')
+
+hidden3.addEventListener('click', function() {
+    selengkapnya3.classList.toggle('slide');
+})
+
+// D
+const hidden4 = document.querySelector('.keterangan-1d input');
+const selengkapnya4 = document.querySelector('.keterangan-2d')
+
+hidden4.addEventListener('click', function() {
+    selengkapnya4.classList.toggle('slide');
+})
+
+// E
+const hidden5 = document.querySelector('.keterangan-1e input');
+const selengkapnya5 = document.querySelector('.keterangan-2e')
+
+hidden5.addEventListener('click', function() {
+    selengkapnya5.classList.toggle('slide');
+})
+
+// F
+const hidden6 = document.querySelector('.keterangan-1f input');
+const selengkapnya6 = document.querySelector('.keterangan-2f')
+
+hidden6.addEventListener('click', function() {
+    selengkapnya6.classList.toggle('slide');
+})
+
+// G
+const hidden7 = document.querySelector('.keterangan-1g input');
+const selengkapnya7 = document.querySelector('.keterangan-2g')
+
+hidden7.addEventListener('click', function() {
+    selengkapnya7.classList.toggle('slide');
+})
+
+// H
+const hidden8 = document.querySelector('.keterangan-1h input');
+const selengkapnya8 = document.querySelector('.keterangan-2h')
+
+hidden8.addEventListener('click', function() {
+    selengkapnya8.classList.toggle('slide');
+})
+
+// I
+const hidden9 = document.querySelector('.keterangan-1i input');
+const selengkapnya9 = document.querySelector('.keterangan-2i')
+
+hidden9.addEventListener('click', function() {
+    selengkapnya9.classList.toggle('slide');
+})
